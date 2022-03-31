6366a8364cb8a9b4e89ed56dfd74a654dfcf24b6
Tomasz Gorochowik <tgorochowik@antmicro.com>
2022-03-31 15:54:55 +0200

    Simplify formats tests
    The new test keeps the same coverage, but parses fake buffers instead of
    the real big images. This makes it pass in fraction of a second instead
    of the original several minutes.

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,50 +1,16 @@
 """Module for testing formats on resources entities"""
 
 from raviewer.src.core import (get_displayable, load_image, parse_image)
-from terminaltables import AsciiTable
+from raviewer.image.image import Image
 from raviewer.image.color_format import AVAILABLE_FORMATS
-import os
-import pkg_resources
-import time
 import pytest
 
 
-@pytest.fixture
-def formats():
-    return AVAILABLE_FORMATS
-
-
-def test_all(formats):
-    """Test all formats"""
-    print("Testing all formats, It may take a while...")
-    table_data = [["Format", "Passed", "Performance"]]
-    start_range = 800
-    end_range = 810
-    for color_format in formats.keys():
-        file_path = pkg_resources.resource_filename('resources',
-                                                    color_format + "_1000_750")
-        passed_results = 0
-        format_performance = 0
-        start = time.time()
-        for width in range(start_range, end_range):
-            try:
-                if not os.path.exists(file_path):
-                    break
-                img = load_image(file_path)
-                img = parse_image(img.data_buffer, color_format, width)
-                get_displayable(img)
-                passed_results += 1
-            except:
-                continue
-        end = time.time()
-        #Stats
-        format_performance = "{:.3f}".format(round(end - start, 3))
-        table_data.append([
-            color_format, "{}/{}".format(passed_results,
-                                         end_range - start_range),
-            format_performance
-        ])
-
-    table = AsciiTable(table_data)
-    table.title = 'Test all formats'
-    print(table.table)
+@pytest.mark.parametrize('fmt', AVAILABLE_FORMATS.keys())
+@pytest.mark.parametrize('width', range(4, 8))
+@pytest.mark.parametrize('height', range(8))
+def test_format_conversion(fmt, width, height):
+    """Test format"""
+    img = Image(bytearray([0] * 64))
+    parsed_img = parse_image(img.data_buffer, fmt, width, height)
+    get_displayable(parsed_img)
