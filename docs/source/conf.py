0bf2e3c5eeae0a6f344ab88906a372f75a08f24d
Tomasz Gorochowik <tgorochowik@antmicro.com>
2023-02-03 14:44:37 +0100

    Docs build cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,19 +46,7 @@ myst_substitutions = {
 
 today_fmt = '%Y-%m-%d'
 
-pygments_style = 'sphinx'
-
-needs_sphinx = '1.8'
-
 todo_include_todos=False
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-source_suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -70,23 +58,9 @@ html_show_sphinx = False
 
 html_theme_options = antmicro_html_theme_options(pdf_url=f"{basic_filename}.pdf")
 
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
 html_title = project
 
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
 html_logo = default_html_logo
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = basic_filename
-
-try: html_context
-except: html_context = {}
-html_context['basic_filename'] = basic_filename
 
 # -- Options for LaTeX output --------------------------------------------------
 
