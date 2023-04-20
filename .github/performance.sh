074891e436e9b92f9a15413fe098432c3f2318db
Krzysztof Wasielewski <kwasielewski@internships.antmicro.com>
2023-04-20 08:18:05 +0200

    [#43338] performance.sh: Updated CI job to use valid arguments
    Tests now run for multiple sizes
    
    Signed-off-by: Krzysztof Wasielewski <kwasielewski@internships.antmicro.com>

--- .github/performance.sh
+++ .github/performance.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 ./tests/performance.py --random --size 1000 750
-python3 ./tests/performance.py --random --size 16000 12000 --count 1 10
+python3 ./tests/performance.py --random --count 10 --size 1000 750 1920 1080 1954 1500 4000 3000 8000 6000 16000 12000
