48f0f69bb5307685f67afacbc47d9c2f12693bb5
Andrii Chmutov <achmutov@internships.antmicro.com>
2024-09-18 14:45:12 +0200

    [#65934] styles_config: font_config: Use importlib.resources instead of deprecated pkg_resources
    Signed-off-by: Andrii Chmutov <achmutov@internships.antmicro.com>

--- raviewer/styles_config/font_config.py
+++ raviewer/styles_config/font_config.py
@@ -1,48 +1,37 @@
 """Set up application's fonts"""
 
 import dearpygui.dearpygui as dpg
-import os
-import pkg_resources
+from importlib import resources as res
 from .. import items
 
 with dpg.font_registry() as font_registry:
-    dpg.add_font(pkg_resources.resource_filename('raviewer',
-                                                 '/fonts/OpenSans-Bold.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-Bold.ttf',
                  16,
                  id=items.fonts.opensans_bold)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-BoldItalic.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-BoldItalic.ttf',
                  16,
                  id=items.fonts.opensans_bolditalic)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-ExtraBold.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-ExtraBold.ttf',
                  16,
                  id=items.fonts.opensans_extrabold)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-ExtraBoldItalic.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-ExtraBoldItalic.ttf',
                  16,
                  id=items.fonts.opensans_extrabolditalic)
-    dpg.add_font(pkg_resources.resource_filename('raviewer',
-                                                 '/fonts/OpenSans-Italic.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-Italic.ttf',
                  16,
                  id=items.fonts.opensans_italic)
-    dpg.add_font(pkg_resources.resource_filename('raviewer',
-                                                 '/fonts/OpenSans-Light.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-Light.ttf',
                  16,
                  id=items.fonts.opensans_light)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-LightItalic.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-LightItalic.ttf',
                  16,
                  id=items.fonts.opensans_lightitalic)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-Regular.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-Regular.ttf',
                  16,
                  id=items.fonts.opensans_regular)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-Semibold.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-Semibold.ttf',
                  16,
                  id=items.fonts.opensans_semibold)
-    dpg.add_font(pkg_resources.resource_filename(
-        'raviewer', '/fonts/OpenSans-SemiboldItalic.ttf'),
+    dpg.add_font(res.files('raviewer') / 'fonts/OpenSans-SemiboldItalic.ttf',
                  16,
                  id=items.fonts.opensans_semibolditalic)
